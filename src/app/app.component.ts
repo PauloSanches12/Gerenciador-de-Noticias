d362b1cb3ce650ef9a93364d64348482b067d39b
Paulo <paulo_sanches2012@hotmail.com>
2019-04-29 19:47:49 -0300

    Gerenciador de Noticias

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { NoticiasService } from './noticias.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  tela = "inicial";
+  autor = null;
+  noticia = null;
+
+
+  avancarPublicacao(){
+    this.tela = "publicar";
+  }
+
+  avancarCadastro(){
+    this.tela = "cadastro";
+    this.autor = null,
+    this.noticia = null
+  }
+
+  avancarEstatisticas(){
+    this.tela = "estatisticas";
+  }
+
+  salvarNoticia(){
+    this.tela = "publicar";
+    this.service.salvar(this.autor, this.noticia);
+  }
+
+
+  verNoticiaPublicada(valor){
+    this.service.verNoticiaPublicada(valor)
+    this.tela = "visualizar";
+  }
+
+
+  constructor(private service: NoticiasService){
+
+  }
+
+
+}
---
+++ src/app/noticias.service.ts
@@ -0,0 +1,86 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoticiasService {
+
+  autores = ["Paulo Sérgio", "Rodrigo", "Carlos", "Lucas"];
+  salvo = [];
+  publicado = false;
+  visualizar = {};
+
+  listaAutores(){
+    return this.autores
+  };
+
+  salvar(autor, noticia){
+    let a = {
+      'id': Math.random(),
+      'autor': autor,
+      'noticia': noticia,
+      'publicado': false,
+      'views': 0
+    }
+    this.salvo.push(a);
+    console.log(this.salvo)
+  }
+
+  listaSalva(){
+    return this.salvo;
+  }
+
+  publicarNoticia(valor){
+    for(let x of this.salvo){
+      if (x.id === valor.id){
+        x.publicado = true;
+      }
+    }
+  }
+
+  verNoticiaPublicada(valor){
+    for(let x of this.salvo){
+      if (x.id === valor.id){
+        x.views++
+        this.visualizar = {
+          'autor': x.autor,
+          'noticia': x.noticia
+        }
+      }
+    }
+  }
+
+
+  noticasPorAutor(){
+    let estatisticas = [];
+    let aux = {};
+    for (let x of this.autores){
+      aux[x] = estatisticas.length;
+      estatisticas.push({autor: x, valor: 0})
+    }
+    for (let z of this.salvo){
+      if( z.publicado === true){
+        estatisticas[aux[z.autor]].valor++;
+      }
+    }
+    return estatisticas
+  }
+
+  AutorMaisPublicou(){
+    let nome = '';
+    let quant = 0;
+    for(let x of this.noticasPorAutor()){
+      if (x.valor > quant){
+        nome = x.autor;
+        quant = x.valor;
+      }
+    }
+    return nome
+  }
+
+  quantidadeDeViws(){
+    return this.salvo.sort(function(a,b){return b.views-a.views})
+  }
+
+  constructor() { }
+}
